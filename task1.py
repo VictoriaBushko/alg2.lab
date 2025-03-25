35e8397e51697779a8bd2a0ef9efb66c37de96d9
VictoriaBushko <vika.bushko23@gmail.com>
2025-03-25 11:46:40 +0200

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def sum_of_depths(root: TreeNode, depth=0) -> int:
+    if root is None:
+        return 0
+    return depth + sum_of_depths(root.left, depth + 1) + sum_of_depths(root.right, depth + 1)
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(13)
+    root.right.right = TreeNode(11)
+    root.right.left = TreeNode(8)
+
+    print(sum_of_depths(root))
